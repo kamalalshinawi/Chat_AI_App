00f76b0527ed30f6ff278a6bfe9f80c3d7b63c49
kamalalshinawi <kamalalshinawi@gmail.com>
2026-03-27 15:13:00 +0200

    chor: add Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
 import CameraGallery from './src/features/ImagePicker/CameraGallery';
+import BootSplash from "react-native-bootsplash";
 
 const App = () => {
+
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
   return <CameraGallery/>;
 };
 
